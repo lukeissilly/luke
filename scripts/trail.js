e59727b79017b585940f46f8c301038ad0f60227
Luke <BoganGuy@proton.me>
2023-12-15 16:33:14 +1100

    and i oop

--- scripts/trail.js
+++ scripts/trail.js
@@ -19,8 +19,8 @@ document.addEventListener("DOMContentLoaded", () => {
         for (let i = 0; i < cursorPositions.length - 1; i++) {
             const line = document.createElement("div");
             line.className = "cursor-line";
-            line.style.left = cursorPositions[i].x + "px";
-            line.style.top = cursorPositions[i].y + "px";
+            line.style.left = `${cursorPositions[i].x}px`;
+            line.style.top = `${cursorPositions[i].y}px`;
             line.style.width = `${Math.abs(cursorPositions[i + 1].x - cursorPositions[i].x)}px`;
             line.style.transform = `rotate(${Math.atan2(cursorPositions[i + 1].y - cursorPositions[i].y, cursorPositions[i + 1].x - cursorPositions[i].x)}rad)`;
             cursorTrail.appendChild(line);
